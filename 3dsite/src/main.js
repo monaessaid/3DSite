7e17e55d28579ca72fd3cb38826b6175b88ff061
Mona <2829922e@student.gla.ac.uk>
2025-01-05 10:25:39 -0800

    color fixed was not rendering before

--- 3dsite/src/main.js
+++ 3dsite/src/main.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
@@ -14,7 +14,7 @@ camera.position.setZ(30);
 renderer.render(scene,camera);
 
 const geometry = new THREE.TorusGeometry(10,3,16,100)
-const material = new THREE.MeshBasicMaterial ({color: blue, wireframe: true});
+const material = new THREE.MeshBasicMaterial ( {color: 0x00d5ff, wireframe: true} );
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus)
