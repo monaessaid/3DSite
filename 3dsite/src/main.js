6092121cfbb1f1f96fb987989caf291b088694da
Mona <2829922e@student.gla.ac.uk>
2025-01-14 09:36:50 -0800

    added lighting and helpers

--- 3dsite/src/main.js
+++ 3dsite/src/main.js
@@ -1,47 +1,71 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+// Sets up the scene 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
 
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize ( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render(scene,camera);
+// Renders the scene and the camera 
+renderer.render(scene, camera);
 
-const torus1 = new THREE.TorusGeometry(10,3,16,100)
-const material = new THREE.MeshBasicMaterial ( {color: 0x00d5ff, wireframe: true} );
+// Creating shapes 
+const torus1 = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshStandardMaterial({ color: 0x85dcff });
 const torus = new THREE.Mesh(torus1, material);
 
-const capsule1 = new THREE.CapsuleGeometry(2,2,5,12);
-const material2 = new THREE.MeshBasicMaterial( {color: 0x00ff00, wireframe: true} ); 
-const capsule = new THREE.Mesh(capsule1, material2);
+// const capsule1 = new THREE.CapsuleGeometry(2, 2, 5, 12);
+// const material2 = new THREE.MeshStandardMaterial({ color: 0x00ff00, wireframe: true });
+// const capsule = new THREE.Mesh(capsule1, material2);
 
-const box1 = new THREE.BoxGeometry(10,10,10);
-const material3 = new THREE.MeshBasicMaterial( {color:0xff0055 , wireframe: true} );
-const box = new THREE.Mesh(box1, material3);
+// const box1 = new THREE.BoxGeometry(10, 10, 10);
+// const material3 = new THREE.MeshStandardMaterial({ color: 0xff0055, wireframe: true });
+// const box = new THREE.Mesh(box1, material3);
 
+// Adding the shapes to the scene 
 scene.add(torus)
-scene.add(capsule)
-scene.add(box)
+// scene.add(capsule)
+// scene.add(box)
+
+// PointLight to focus on a specific point 
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5, 5, 5)
+
+// Lighting across the entire scene
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight)
+
+// Helper functions 
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
+
+// Allows you to move the scene with your mouse
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
     requestAnimationFrame(animate);
 
     torus.rotation.x += 0.01;
-    torus.rotation.y += 0.02;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
 
-    capsule.rotation.x += 0.02;
-    capsule.rotation.y += 0.01;
+    // capsule.rotation.x += 0.02;
+    // capsule.rotation.y += 0.01;
 
-    box.rotation.x += 0.02;
-    box.rotation.y += 0.01;
+    // box.rotation.x += 0.03;
+    // box.rotation.y += 0.03;
 
-    renderer.render(scene,camera);
+    controls.update();
+
+    renderer.render(scene, camera);
 }
 
 animate()
